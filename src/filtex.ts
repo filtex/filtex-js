b05e793b4b06d221e0e497f59da7e1b27f7bc065
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-01 23:43:02 +0300

    feat: add parsing and validating funcs to filtex type

--- src/filtex.ts
+++ src/filtex.ts
@@ -1,5 +1,9 @@
 import {Metadata, Lookup, Field} from "./models";
+import {Expression} from "./expressions";
 import {Option, FieldOption, LookupOption} from "./options";
+import {JsonQueryParser, TextQueryParser} from "./parsers";
+import {JsonQueryTokenizer, TextQueryTokenizer} from "./tokenizers";
+import {JsonQueryValidator, TextQueryValidator} from "./validators";
 
 export class Filtex {
     private constructor(private _metadata: Metadata) {
@@ -27,5 +31,21 @@ export class Filtex {
 
     public metadata(): Metadata {
         return this._metadata;
+    }
+
+    public expressionFromJson(jsonQuery: string): Expression {
+        return new JsonQueryParser(this._metadata, new JsonQueryTokenizer(this._metadata)).parse(jsonQuery);
+    }
+
+    public expressionFromText(textQuery: string): Expression {
+        return new TextQueryParser(this._metadata, new TextQueryTokenizer(this._metadata)).parse(textQuery);
+    }
+
+    public validateFromJson(jsonQuery: string) {
+        new JsonQueryValidator(this._metadata, new JsonQueryTokenizer(this._metadata)).validate(jsonQuery);
+    }
+
+    public validateFromText(textQuery: string) {
+        new TextQueryValidator(this._metadata, new TextQueryTokenizer(this._metadata)).validate(textQuery);
     }
 }
