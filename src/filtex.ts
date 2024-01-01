3bb486280ef4096ac8c0991c5eea9c24944e0e51
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-01 23:24:07 +0300

    feat: create filtex type and ctor with options

--- src/filtex.ts
+++ src/filtex.ts
@@ -1,3 +1,31 @@
-export class Filtex {
+import {Metadata, Lookup, Field} from "./models";
+import {Option, FieldOption, LookupOption} from "./options";
 
+export class Filtex {
+    private constructor(private _metadata: Metadata) {
+    }
+
+    public static new(...options: Option[]): Filtex {
+        const lookups = new Map<string, Lookup[]>();
+
+        options.filter(x => x instanceof LookupOption).forEach(x => {
+            const build = (<LookupOption>x).build();
+            build.forEach((value, key) => {
+                lookups.set(key, value);
+            });
+        });
+
+        const fields: Field[] = [];
+
+        options.filter(x => x instanceof FieldOption).forEach(x => {
+            const build = (<FieldOption>x).build(lookups);
+            fields.push(build);
+        });
+
+        return new Filtex(new Metadata(fields));
+    }
+
+    public metadata(): Metadata {
+        return this._metadata;
+    }
 }
