68aad00360088fa19adb1d9faae0b11d09563914
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 15:19:32 +0300

    feat: export filter builders

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { Metadata, Lookup } from './models';
 export { FieldOption, LookupOption } from './options';
+export { MemoryFilterBuilder } from './builders/memory/memoryFilterBuilder';
+export { MongoFilterBuilder } from './builders/mongo/mongoFilterBuilder';
+export { PostgresFilterBuilder } from './builders/postgres/postgresFilterBuilder';
 export { Filtex } from './filtex';
