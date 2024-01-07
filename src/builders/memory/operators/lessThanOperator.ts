a2ba1e1d5b353643efe5c54668115c8aa458b563
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 14:31:26 +0300

    feat: create memory less and less equal operator impls

---
+++ src/builders/memory/operators/lessThanOperator.ts
@@ -0,0 +1,31 @@
+import {FieldType} from "../../../constants";
+import {MemoryExpression} from "../types/memoryExpression";
+import {date, dateTime, number, time} from "../../../utils";
+
+export class LessThanOperator {
+    static build(fieldType: FieldType, field: string, value: any): MemoryExpression {
+        return new MemoryExpression(function (data: { [key: string]: any }): boolean {
+            if (data[field] == null) {
+                return false;
+            }
+
+            if (fieldType === FieldType.FieldTypeNumber) {
+                return number(data[field]) < number(value);
+            }
+
+            if (fieldType === FieldType.FieldTypeDate) {
+                return date(data[field]) < date(value);
+            }
+
+            if (fieldType === FieldType.FieldTypeTime) {
+                return time(data[field]) < time(value);
+            }
+
+            if (fieldType === FieldType.FieldTypeDateTime) {
+                return dateTime(data[field]) < dateTime(value);
+            }
+
+            return false;
+        });
+    }
+}
