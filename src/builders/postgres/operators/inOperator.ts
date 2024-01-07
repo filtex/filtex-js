1041a4f383785247b4088d2818a73b7a5af054c9
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 15:16:25 +0300

    feat: create postgres in and not in operator impls

---
+++ src/builders/postgres/operators/inOperator.ts
@@ -0,0 +1,33 @@
+import {PostgresExpression} from "../types/postgresExpression";
+import {FieldType} from "../../../constants";
+import {isArray} from "../../../utils";
+
+export class InOperator {
+    static build(fieldType: FieldType, field: string, value: any, index: number): PostgresExpression | null {
+        if (fieldType.isArray() || value === null) {
+            return null;
+        }
+
+        if (!isArray(value)) {
+            if (fieldType === FieldType.FieldTypeString) {
+                return new PostgresExpression(`LOWER(${field}) IN (LOWER($${index}))`, [value]);
+            } else {
+                return new PostgresExpression(`${field} IN ($${index})`, [value]);
+            }
+        }
+
+        if (fieldType === FieldType.FieldTypeString) {
+            const indexes = [];
+            for (let i = index; i < index + value.length; i++) {
+                indexes.push(`LOWER($${i})`);
+            }
+            return new PostgresExpression(`LOWER(${field}) IN (${indexes.join(",")})`, value);
+        } else {
+            const indexes = [];
+            for (let i = index; i < index + value.length; i++) {
+                indexes.push(`$${i}`);
+            }
+            return new PostgresExpression(`${field} IN (${indexes.join(",")})`, value);
+        }
+    }
+}
