1180ba72be7a82b0d81ba5be862a35f997cf3039
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 14:32:47 +0300

    feat: create memory blank and not blank operator impls

---
+++ src/builders/memory/operators/notBlankOperator.ts
@@ -0,0 +1,18 @@
+import {FieldType} from "../../../constants";
+import {MemoryExpression} from "../types/memoryExpression";
+
+export class NotBlankOperator {
+    static build(fieldType: FieldType, field: string, value: any): MemoryExpression {
+        return new MemoryExpression(function (data: { [key: string]: any }): boolean {
+            if (fieldType.isArray()) {
+                return data[field] != null && data[field].length > 0;
+            }
+
+            if (fieldType === FieldType.FieldTypeString) {
+                return data[field] != null && data[field].toString().length > 0;
+            }
+
+            return false;
+        });
+    }
+}
