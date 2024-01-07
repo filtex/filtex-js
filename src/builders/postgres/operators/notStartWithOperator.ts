592765f8e0c1d7ab8241be6b4555e28de69f8bbf
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 15:13:54 +0300

    feat: create postgres start with and not start with operator impls

---
+++ src/builders/postgres/operators/notStartWithOperator.ts
@@ -0,0 +1,12 @@
+import {PostgresExpression} from "../types/postgresExpression";
+import {FieldType} from "../../../constants";
+
+export class NotStartWithOperator {
+    static build(fieldType: FieldType, field: string, value: any, index: number): PostgresExpression | null {
+        if (fieldType != FieldType.FieldTypeString) {
+            return null;
+        }
+
+        return new PostgresExpression(`${field} NOT ILIKE $${index} || '%'`, [value]);
+    }
+}
