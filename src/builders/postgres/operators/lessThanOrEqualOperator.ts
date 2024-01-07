522cb27cf61fc7616886eed0cf94a61653d92d6a
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 15:14:54 +0300

    feat: create postgres less and less equal operator impls

---
+++ src/builders/postgres/operators/lessThanOrEqualOperator.ts
@@ -0,0 +1,15 @@
+import {PostgresExpression} from "../types/postgresExpression";
+import {FieldType} from "../../../constants";
+
+export class LessThanOrEqualOperator {
+    static build(fieldType: FieldType, field: string, value: any, index: number): PostgresExpression | null {
+        if (fieldType != FieldType.FieldTypeNumber &&
+            fieldType != FieldType.FieldTypeDate &&
+            fieldType != FieldType.FieldTypeTime &&
+            fieldType != FieldType.FieldTypeDateTime) {
+            return null;
+        }
+
+        return new PostgresExpression(`${field} <= $${index}`, [value]);
+    }
+}
