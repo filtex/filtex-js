dd3b553fccd73252879e624d26221e8d011ce764
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 14:29:26 +0300

    feat: create memory equal and not equal operator impls

---
+++ src/builders/memory/operators/notEqualOperator.ts
@@ -0,0 +1,15 @@
+import {FieldType} from "../../../constants";
+import {MemoryExpression} from "../types/memoryExpression";
+import {checkEquality} from "../utils/memoryUtil";
+
+export class NotEqualOperator {
+    static build(fieldType: FieldType, field: string, value: any): MemoryExpression {
+        return new MemoryExpression(function (data: { [key: string]: any }): boolean {
+            if (fieldType.isArray() || data[field] === null || value === null) {
+                return false;
+            }
+
+            return !checkEquality(fieldType, data[field], value);
+        });
+    }
+}
