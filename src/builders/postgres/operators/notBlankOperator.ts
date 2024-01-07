a1c2fcce015f9fc7be3831f980fee1bbad779e90
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 15:15:54 +0300

    feat: create postgres blank and not blank operator impls

---
+++ src/builders/postgres/operators/notBlankOperator.ts
@@ -0,0 +1,16 @@
+import {PostgresExpression} from "../types/postgresExpression";
+import {FieldType} from "../../../constants";
+
+export class NotBlankOperator {
+    static build(fieldType: FieldType, field: string, value: any, index: number): PostgresExpression | null {
+        if (fieldType.isArray()) {
+            return new PostgresExpression(`ARRAY_LENGTH(${field}, 1) <> 0`, []);
+        }
+
+        if (fieldType == FieldType.FieldTypeString) {
+            return new PostgresExpression(`${field} IS NOT NULL AND ${field} <> ''`, []);
+        }
+
+        return null;
+    }
+}
