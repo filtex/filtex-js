79b019cf3b72360e56f25f7db3d85056e6b09a9f
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 14:55:22 +0300

    feat: create memory contain and not contain operator impls

---
+++ src/builders/memory/operators/containOperator.ts
@@ -0,0 +1,20 @@
+import {FieldType} from "../../../constants";
+import {MemoryExpression} from "../types/memoryExpression";
+import {checkEquality} from "../utils/memoryUtil";
+
+export class ContainOperator {
+    static build(fieldType: FieldType, field: string, value: any): MemoryExpression {
+        if (fieldType.isArray()) {
+            return new MemoryExpression(function (data: { [key: string]: any }): boolean {
+                return data[field]?.some((v: any) => checkEquality(fieldType, v, value)) || false;
+            });
+        }
+
+        return new MemoryExpression(function (data: { [key: string]: any }): boolean {
+            if (fieldType == FieldType.FieldTypeString) {
+                return data[field]?.toString()?.toLowerCase().includes(value?.toString()?.toLowerCase()) === true;
+            }
+            return false;
+        });
+    }
+}
