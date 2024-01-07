5778ebc51ff2dfc7ade8b111912add640ba1e0a3
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 15:15:39 +0300

    test: add postgres greater and greater equal operator tests

---
+++ test/builders/postgres/operators/greaterThanOperator.spec.ts
@@ -0,0 +1,148 @@
+import {GreaterThanOperator} from "../../../../src/builders/postgres/operators/greaterThanOperator";
+import {FieldType} from "../../../../src/constants";
+
+describe('greaterThanOperator', () => {
+    it('should return expression when field type is number', () => {
+        // Arrange
+        const value = 100.0;
+
+        // Act
+        const expression = GreaterThanOperator.build(FieldType.FieldTypeNumber, 'Value', value, 1);
+
+        // Assert
+        expect(expression).not.toBeNull();
+        if (expression) {
+            expect(expression.condition).toEqual("Value > $1");
+            expect(expression.args).toHaveLength(1);
+            expect(expression.args[0]).toEqual(value);
+        }
+    });
+
+    it('should return expression when field type is date', () => {
+        // Arrange
+        const value = new Date();
+
+        // Act
+        const expression = GreaterThanOperator.build(FieldType.FieldTypeDate, 'Value', value, 1);
+
+        // Assert
+        expect(expression).not.toBeNull();
+        if (expression) {
+            expect(expression.condition).toEqual("Value > $1");
+            expect(expression.args).toHaveLength(1);
+            expect(expression.args[0]).toEqual(value);
+        }
+    });
+
+    it('should return expression when field type is time', () => {
+        // Arrange
+        const value = 1000;
+
+        // Act
+        const expression = GreaterThanOperator.build(FieldType.FieldTypeTime, 'Value', value, 1);
+
+        // Assert
+        expect(expression).not.toBeNull();
+        if (expression) {
+            expect(expression.condition).toEqual("Value > $1");
+            expect(expression.args).toHaveLength(1);
+            expect(expression.args[0]).toEqual(value);
+        }
+    });
+
+    it('should return expression when field type is datetime', () => {
+        // Arrange
+        const value = new Date();
+
+        // Act
+        const expression = GreaterThanOperator.build(FieldType.FieldTypeDateTime, 'Value', value, 1);
+
+        // Assert
+        expect(expression).not.toBeNull();
+        if (expression) {
+            expect(expression.condition).toEqual("Value > $1");
+            expect(expression.args).toHaveLength(1);
+            expect(expression.args[0]).toEqual(value);
+        }
+    });
+
+    it('should return null when field type is string', () => {
+        // Arrange
+
+        // Act
+        const expression = GreaterThanOperator.build(FieldType.FieldTypeString, 'Value', null, 0);
+
+        // Assert
+        expect(expression).toBeNull();
+    });
+
+    it('should return null when field type is string array', () => {
+        // Arrange
+
+        // Act
+        const expression = GreaterThanOperator.build(FieldType.FieldTypeStringArray, 'Value', null, 0);
+
+        // Assert
+        expect(expression).toBeNull();
+    });
+
+    it('should return null when field type is number array', () => {
+        // Arrange
+
+        // Act
+        const expression = GreaterThanOperator.build(FieldType.FieldTypeNumberArray, 'Value', null, 0);
+
+        // Assert
+        expect(expression).toBeNull();
+    });
+
+    it('should return null when field type is boolean', () => {
+        // Arrange
+
+        // Act
+        const expression = GreaterThanOperator.build(FieldType.FieldTypeBoolean, 'Value', null, 0);
+
+        // Assert
+        expect(expression).toBeNull();
+    });
+
+    it('should return null when field type is boolean array', () => {
+        // Arrange
+
+        // Act
+        const expression = GreaterThanOperator.build(FieldType.FieldTypeBooleanArray, 'Value', null, 0);
+
+        // Assert
+        expect(expression).toBeNull();
+    });
+
+    it('should return null when field type is date array', () => {
+        // Arrange
+
+        // Act
+        const expression = GreaterThanOperator.build(FieldType.FieldTypeDateArray, 'Value', null, 0);
+
+        // Assert
+        expect(expression).toBeNull();
+    });
+
+    it('should return null when field type is time array', () => {
+        // Arrange
+
+        // Act
+        const expression = GreaterThanOperator.build(FieldType.FieldTypeTimeArray, 'Value', null, 0);
+
+        // Assert
+        expect(expression).toBeNull();
+    });
+
+    it('should return null when field type is datetime array', () => {
+        // Arrange
+
+        // Act
+        const expression = GreaterThanOperator.build(FieldType.FieldTypeDateTimeArray, 'Value', null, 0);
+
+        // Assert
+        expect(expression).toBeNull();
+    });
+});
