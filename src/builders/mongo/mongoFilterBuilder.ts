d43cdfca3f237ee36ed11ae60cc40f7b2f4f60e8
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 14:58:44 +0300

    feat: create mongo expression type and build func

--- src/builders/mongo/mongoFilterBuilder.ts
+++ src/builders/mongo/mongoFilterBuilder.ts
@@ -1,3 +1,9 @@
-export class MongoFilterBuilder {
+import {MongoExpression} from "./types/mongoExpression";
+import {Expression} from "../../expressions";
+import {newCouldNotBeBuiltError} from "../../errors";
 
+export class MongoFilterBuilder {
+    public build(expression: Expression): MongoExpression {
+        throw newCouldNotBeBuiltError();
+    }
 }
---
+++ src/builders/mongo/types/mongoExpression.ts
@@ -0,0 +1,5 @@
+export class MongoExpression {
+    constructor(
+        public condition: any
+    ) { }
+}
